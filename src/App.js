6c6d3859e21417bfc350e332ab2bb881bfc082a6
s-nguza <siyabonganguza18@gmail.com>
2025-07-14 11:32:01 +0200

    added intro and paragraph

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Navbar from "./components/navbar";
+import Intro from "./components/intro/intro";
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Intro/>
       
     </div>
   );
---
+++ src/components/intro/intro.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './intro.css';
+import myimg from '../../asserts/myimage.jpeg';
+import {Link} from 'react-scroll';
+import btnimage from '../../asserts/hireme.png'
+
+const Intro = () => {
+  return (
+    <section id ='intro'>
+        <div className=' introContent'>
+            <span className='hello'> Hello,</span>
+            <span className='introtext'> I'm<span className='introName'> Siyabonga</span><br/>fullstack developer</span>
+            <p className = "introPara"> I am a skilled fullstack dev with with experience in creating visually appealing websites</p>
+            <Link><button className='btn'><img src={btnimage} alt ='Hire' className='btnImg' />Hire me </button></Link>
+            </div>
+        <img src ={myimg} alt ="Profile" className='myimg'/>
+
+    
+
+    </section> 
+  )
+}
+
+export default Intro
