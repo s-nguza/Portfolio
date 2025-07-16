79941822aef66bb0af677bf0a7bed2529f25b238
s-nguza <siyabonganguza18@gmail.com>
2025-07-16 11:43:25 +0200

    contact changes

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,21 +1,25 @@
 import React from 'react'
-import './contact.css'
+import './contact.css';
 
 const Contact = () => {
   return (
-    <section className='contactPage'>
-        <div id ="contact" ></div>
-        <h1 className='contactheader'>Contactme</h1>
-        <span className='contactDesc'>Please fill out the form below to discuss any work opportunities.</span>
-        <form className='contactForm'>
-            <input type='text' className='name' placeholder='Your name'/>
-            <input type = 'email' className='email' placeholder='Your email'/>
-            <textarea className='msg' name='message'  rows = '5' placeholder='Your Message'></textarea>
-            <button type='submit' value='Send' className='submitBtn'>Submit</button>     
-        </form>
-    
+    <section id='contactPage'>
+      <div className="contact">
+        <div id="contactContent">
+          <h1 className='contactheader'>Contact Me</h1>
+          <span className='contactDesc'>
+            Please fill out the form below to discuss any work opportunities.
+          </span>
+          <form className='contactForm'>
+            <input type='text' className='name' placeholder='Your name' />
+            <input type='email' className='email' placeholder='Your email' />
+            <textarea className='msg' name='message' rows='5' placeholder='Your Message'></textarea>
+            <button type='submit' className='submitBtn'>Submit</button>
+          </form>
+        </div>
+      </div>
     </section>
   )
 }
 
-export default Contact
+export default Contact;
