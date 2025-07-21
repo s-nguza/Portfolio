397ba268fde78ae561a19778c552d0a83729ae1f
s-nguza <siyabonganguza18@gmail.com>
2025-07-21 09:34:50 +0200

    added the burger menu

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './navbar.css'
 import Alphs from '../../asserts/AlphS.png';
 import {Link} from 'react-scroll';
 import contactImg from '../../asserts/contact.png'
+import menu from '../../asserts/menu.png';
 
 const Navbar = () => {
+  const [showMenu,setShowMenu] = useState(false);
   return (
     <nav className='navbar'>
     <img src = {Alphs} alt="Alphs" className= 'Alphs'/>
@@ -18,8 +20,16 @@ const Navbar = () => {
     <button className='desktopMenuBtn' onClick={()=>{
       document.getElementById('contactPage').scrollIntoView({behavior: 'smooth'});
     }}>
-      <img src={contactImg} alt ="contact" className='desktopMenuImg'/>Contact Me</button>    
-      
+      <img src={contactImg} alt ="contact" className='desktopMenuImg'/>Contact Me</button>
+    <img src = {menu} alt="BMenu" className= 'mobMenu' onClick={()=>setShowMenu(!showMenu)}/>
+    <div className='navmenu' style = {{display:showMenu? 'flex':'none'}}>
+      <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500} className='ListItem' onClick={()=>setShowMenu(false)}>Home</Link>
+      <Link activeClass='active' to='about-section' spy={true} smooth={true} offset={-100} duration={500} className='ListItem' onClick={()=>setShowMenu(false)}>About</Link>
+      <Link activeClass='active' to='projects-section' spy={true} smooth={true} offset={-100} duration={500} className='ListItem' onClick={()=>setShowMenu(false)}>Projects</Link>
+      <Link activeClass='active' to='contactPage' spy={true} smooth={true} offset={-100} duration={500} className='ListItem' onClick={()=>setShowMenu(false)}>Contact</Link>
+
+    </div>    
+        
       </nav>
     
 )}
