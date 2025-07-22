1f6b457478b37d889d3e886c106b6046f968b069
s-nguza <siyabonganguza18@gmail.com>
2025-07-22 11:24:15 +0200

    emailjs

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './contact.css';
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_gbirkyo','template_fjnokaa',form.current,'9Z_sE9UYSgt4A7fcF')
+      .then((result) => {
+        console.log(result.text);
+      }, (error) =>{
+        console.log(error.text);
+      });
+    }
   return (
     <section id='contactPage'>
       <div className="contact">
@@ -10,10 +22,10 @@ const Contact = () => {
           <span className='contactDesc'>
             Please fill out the form below to discuss any work opportunities.
           </span>
-          <form className='contactForm'>
-            <input type='text' className='name' placeholder='Your name' />
-            <input type='email' className='email' placeholder='Your email' />
-            <textarea className='msg' name='message' rows='5' placeholder='Your Message'></textarea>
+          <form className='contactForm' ref={form} onSubmit={sendEmail} >
+            <input type='text' className='name' placeholder='Your name' name = 'your_name' />
+            <input type='email' className='email' placeholder='Your email' name = 'your_email' />
+            <textarea className='msg' name='message' rows='5' placeholder='Your Message' ></textarea>
             <button type='submit' className='submitBtn'>Submit</button>
           </form>
         </div>
--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -1,6 +1,6 @@
 import React from 'react';
+import profilepic from '../../asserts/ThisMe.jpg';
 import './intro.css';
-// import myimg from '../../asserts/myimage.jpeg';
 import {Link} from 'react-scroll';
 import btnimage from '../../asserts/hireme.png'
 
@@ -13,7 +13,11 @@ const Intro = () => {
             <p className = "introPara"> I am a skilled fullstack dev with with experience in creating <br/> visually appealing websites</p>
             <Link><button className='btn'><img src={btnimage} alt ='Hire me' className='btnImg' />Hire me </button></Link>
         </div>
-        {/* <img src ={myimg} alt ="Profile" className='myimg'/> */}
+
+        <div className="introImgContainer">
+          <img src={profilepic} alt="Siyabonga" className="introImg" />
+        </div>
+      
 
     
 
